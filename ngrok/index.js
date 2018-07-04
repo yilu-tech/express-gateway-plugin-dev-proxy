5956895e33dc8b7ec41754dfcaaa4d28d025ecdc
leeke <lishu.priv@gmail.com>
2018-07-04 11:15:44 +0800

    Update index.js
    添加运行提示

--- ngrok/index.js
+++ ngrok/index.js
@@ -14,7 +14,7 @@ exports.start = function(){
     }
 
     const ngrokd = spawn(command,['-tlsKey='+key,'-tlsCrt='+crt,'-domain=gw.proxy','-httpAddr=:10803','-httpsAddr=:10804','-tunnelAddr=:10805']);
-    
+    console.info(`Ngrokd in runing on http:10803 https:10804 tunnel:10805`);
     
     ngrokd.stdout.on('data', (data) => {
       // console.info(`stdout: ${data}`);
